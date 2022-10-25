8e2401de1d225170abc09df7b3affa7825d3d47f
Ivan Baturkin <ivanmysterys@gmail.com>
2022-10-25 23:09:29 +0300

    test: verify allocator usage

--- src/main.c
+++ src/main.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-// #include <stdlib.h>
 
-#include "allocator_impl.h"
+#include "allocator.h"
 
 int main()
 {
-    for (size_t i = 1; i < 50; ++i)
-    {
-        printf("%zu -> %zu\n", i, ROUND_BYTES(i));
-    }
+    mem_alloc(222);
+    mem_show();
 }
