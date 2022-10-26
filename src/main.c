082a48e0468ae18a6bf5a9c9f81dfe05f8e76560
Ivan Baturkin <ivanmysterys@gmail.com>
2022-10-26 23:51:48 +0300

    feat: implement allocated memory random filing

--- src/main.c
+++ src/main.c
@@ -2,8 +2,34 @@
 
 #include "allocator.h"
 
+static void bufFill(char* bytes, size_t size)
+{
+    for (size_t i = 0; i < size; ++bytes, ++i)
+    {
+        *bytes = (char)rand();
+    }
+}
+
+static void* bufAlloc(size_t size)
+{
+    void* ptr = NULL;
+    ptr = mem_alloc(size);
+    if (ptr != NULL) bufFill(ptr, size);
+
+    return ptr;
+}
+
 int main()
 {
-    mem_alloc(222);
-    mem_show();
+    bufAlloc(10);
+    mem_show("mem_alloc(10):");
+
+    bufAlloc(16);
+    mem_show("mem_alloc(16):");
+
+    bufAlloc(100);
+    mem_show("mem_alloc(100):");
+
+    bufAlloc(225);
+    mem_show("mem_alloc(225):");
 }
