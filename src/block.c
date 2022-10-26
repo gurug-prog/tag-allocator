1e222af372f1cbfd8acf08ee3b7a5d987803416e
Ivan Baturkin <ivanmysterys@gmail.com>
2022-10-26 22:44:06 +0300

    feat: add block fields setters

--- src/block.c
+++ src/block.c
@@ -17,9 +17,19 @@ size_t Block_getCurrBlockSize(const Block* self)
     return self->currBlockSize;
 }
 
+void Block_setCurrBlockSize(Block* self, size_t size)
+{
+    self->currBlockSize = size;
+}
+
 size_t Block_getPrevBlockSize(const Block* self)
 {
     return self->prevBlockSize;
+}
+
+void Block_setPrevBlockSize(Block* self, size_t size)
+{
+    self->prevBlockSize = size;
 }
 
 bool Block_isBusy(const Block* self)
@@ -27,14 +37,29 @@ bool Block_isBusy(const Block* self)
     return self->isBusy;
 }
 
+void Block_setIsBusy(Block* self, bool value)
+{
+    self->isBusy = value;
+}
+
 bool Block_isFirst(const Block* self)
 {
     return self->isFirst;
 }
 
+void Block_setIsFirst(Block* self, bool value)
+{
+    self->isFirst = value;
+}
+
 bool Block_isLast(const Block* self)
 {
     return self->isLast;
+}
+
+void Block_setIsLast(Block* self, bool value)
+{
+    self->isLast = value;
 }
 
 void Block_init(Block* self, size_t size)
