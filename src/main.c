003cc2439bdb1ef9ed6f74cd8b10b65c3f13acd8
Ivan Baturkin <ivanmysterys@gmail.com>
2022-10-27 20:03:47 +0300

    test: add alloc man-in-middle test

--- src/main.c
+++ src/main.c
@@ -3,15 +3,33 @@
 #include "buffer_proccesor.h"
 #include "block_info.h"
 
+void freeGarbage(void** pointers, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        bufFree(pointers[i]);
+    }
+}
+
+void test_Alloc3zonesFreeMiddle_Result_MiddleFreedOthersNotAffected()
+{
+    void* p1 = bufAlloc(1);
+    bufShow("memAlloc(p1) [orig-size: 1]");
+
+    void* p2 = bufAlloc(100);
+    bufShow("memAlloc(p2) [orig-size: 100]");
+
+    void* p3 = bufAlloc(30);
+    bufShow("memAlloc(p2) [orig-size: 30]");
+
+    bufFree(p2);
+    bufShow("memFree(p2)");
+
+    freeGarbage((void*[]){p1, p3}, 2);
+}
+
 int main()
 {
-    void* p1 = bufAlloc(100);
-    bufShow("memAlloc(p1):");
+    test_Alloc3zonesFreeMiddle_Result_MiddleFreedOthersNotAffected();
 
-    bufAlloc(10);
-    p1 = bufRealloc(p1, 100);
-    bufShow("memRealloc(p1)");
-
-    p1 = bufRealloc(p1, 1000);
-    bufShow("memRealloc(p1)");
 }
